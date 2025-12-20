3e477dca89f2d5249b3f9fc22974ddabe2329c74
Derek Wan <d.wan@icloud.com>
2025-12-20 17:51:52 +0900

    asdf

--- install.sh
+++ install.sh
@@ -6,10 +6,12 @@ echo_date() { echo "[$(date +'%Y-%m-%d %H:%M:%S')] $*" >&2; }
 # main
 echo_date "Running installer..."
 
-PATH="${HOME}/.local/bin${PATH:+:${PATH}}"
-
+LOCAL_BIN="${HOME}/.local/bin"
 if [ $(hostname) = RH-MacBook ]; then
     echo_date "This is 'RH-MacBook'; running installer for 'RH-MacBook'..."
     ./scripts/setup-uv.sh
-    ./scripts/uv/rh-macbook.py
+    PATH="${LOCAL_BIN}${PATH:+:${PATH}}" ./scripts/rh-macbook/script.py
+    echo_date "Finished running installer for 'RH-MacBook'..."
+else
+    echo_date "Unknown hostname '$(hostname)'; exiting..." && exit 1
 fi
--- scripts/rh-macbook/script.py
+++ scripts/rh-macbook/script.py
@@ -25,7 +25,7 @@ from re import escape, search
 from string import Template
 from subprocess import CalledProcessError, check_call, check_output
 from typing import TYPE_CHECKING, Any, Literal, assert_never
-
+from string import Template
 import tomlkit
 import yaml
 from click import command
@@ -65,14 +65,41 @@ _LOGGER = getLogger(__name__)
 @command(**CONTEXT_SETTINGS)
 def _main()->None:
     _LOGGER.info("Running 'RH-MacBook' installer...")
+    _install_bump_my_version()
     _install_pre_commit()
+    _LOGGER.info("Finished running 'RH-MacBook' installer")
+
+
+def _install_bump_my_version()->None:
+    if which('bump-my-version') is None:
+         _LOGGER.info("Installing 'bump-my-version'...")
+         run('uv', 'tool', 'install', 'bump-my-version', print=True)
+    else:
+         _LOGGER.info("'pre-commit' is already installed")
+
 
 def _install_pre_commit()->None:
     if which('pre-commit') is None:
          _LOGGER.info("Installing 'pre-commit'...")
-         run('uv', 'tool', 'install', 'pre-commit')
+         run('uv', 'tool', 'install', 'pre-commit', print=True)
+    else:
+         _LOGGER.info("'pre-commit' is already installed")
 
 
 if __name__ == "__main__":
     basic_config(obj=__name__)
     _main()
+
+################ utilities ####################
+
+@contextmanager
+def yield_github_latest_download(
+    owner: str, repo: str, filename_template: str, /
+) -> Iterator[Path]:
+    tag = get_latest_tag(owner, repo)
+    filename = Template(filename_template).substitute(
+        tag=tag, tag_without_v=tag.lstrip("v")
+    )
+    url = f"https://github.com/{owner}/{repo}/releases/download/{tag}/{filename}"
+    with yield_download(url) as temp_file:
+        yield temp_file
