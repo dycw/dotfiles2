913bb7c1b104b4a6b7627c25642173e6f345d1ff
Derek Wan <d.wan@icloud.com>
2025-12-31 15:52:32 +0900

    2025-12-31 15:52:32 (Wed)  > DW-Swift  > derek

---
+++ scripts/dw-swift/script.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env -S uv run --script
+# /// script
+# requires-python = ">=3.14"
+# dependencies = [
+#   "click >= 8.3.1, < 8.4",
+#   "dycw-utilities >= 0.170.1, < 0.172",
+#   "pytest-xdist >= 3.8.0, < 3.9",
+#   "pyyaml >= 6.0.3, < 6.1",
+#   "rich >= 14.2.0, < 14.3",
+#   "tomlkit >= 0.13.3, < 0.14",
+#   "typed-settings[attrs, click] >= 25.3.0, < 25.4",
+#   "xdg-base-dirs >= 6.0.2, < 6.1",
+# ]
+# ///
+
+from __future__ import annotations
