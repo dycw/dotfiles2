2883330acbeb1d38dbfcf9c32993d9f807c21081
Derek Wan <d.wan@icloud.com>
2025-12-31 15:48:45 +0900

    2025-12-31 15:48:45 (Wed)  > DW-Swift  > derek

--- install.sh
+++ install.sh
@@ -7,11 +7,20 @@ echo_date() { echo "[$(date +'%Y-%m-%d %H:%M:%S')] $*" >&2; }
 echo_date "Running installer..."
 
 LOCAL_BIN="${HOME}/.local/bin"
-if [ $(hostname) = RH-MacBook ]; then
+case "$(hostname)" in
+DW-Swift)
+    echo_date "This is 'DW-Swift'; running installer for 'DW-Swift'..."
+    ./scripts/setup-uv.sh
+    PATH="${LOCAL_BIN}${PATH:+:${PATH}}" ./scripts/dw-swift/script.py
+    echo_date "Finished running installer for 'DW-Swift'..."
+    ;;
+RH-MacBook)
     echo_date "This is 'RH-MacBook'; running installer for 'RH-MacBook'..."
     ./scripts/setup-uv.sh
     PATH="${LOCAL_BIN}${PATH:+:${PATH}}" ./scripts/rh-macbook/script.py
     echo_date "Finished running installer for 'RH-MacBook'..."
-else
+    ;;
+*)
     echo_date "Unknown hostname '$(hostname)'; exiting..." && exit 1
-fi
+    ;;
+esac
