9be31e29339b5fadcff554365934cc5b16909ac4
Derek Wan <d.wan@icloud.com>
2026-02-20 12:12:20 +0900

    Add `keymapp` (#28)
    Co-authored-by: github-actions-bot <noreply@github.com>

---
+++ configs/keymapp/set-up-manual.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+echo "[$(date '+%Y-%m-%d %H:%M:%S')] Setting up 'keymapp'..."
+
+TEMP_DIR="$(mktemp -d)"
+trap 'rm -rf "${TEMP_DIR}"' EXIT
+
+URL='https://oryx.nyc3.cdn.digitaloceanspaces.com/keymapp/keymapp-latest.tar.gz'
+curl -fsSL "${URL}" | tar -xz -C "${TEMP_DIR}"
+
+install -Dm755 "${TEMP_DIR}/keymapp" "${HOME}/.local/bin/keymapp"
+
+echo "[$(date '+%Y-%m-%d %H:%M:%S')] Finished setting up 'keymapp'"
