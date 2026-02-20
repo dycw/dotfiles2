f27159f5eb43210567db22832802eadcb2d4cc42
Derek Wan <d.wan@icloud.com>
2026-02-20 15:11:35 +0900

    Handle `fdfind` and `batcat` (#46)
    Co-authored-by: github-actions-bot <noreply@github.com>

--- configs/bat/shell.fish
+++ configs/bat/shell.fish
@@ -1,15 +1,33 @@
 #!/usr/bin/env fish
 
-if not status is-interactive; or not type -q bat
+if not status is-interactive; or not begin
+        type -q bat; or type -q batcat
+    end
     exit
 end
 
 ###############################################################################
 
+if type -q batcat and not type -q bat
+    function bat
+        __bat $argv
+    end
+end
+
 function cat
-    bat $argv
+    __bat $argv
 end
 
 function catp
-    bat --style=plain $argv
+    __bat --style=plain $argv
+end
+
+function __bat
+    if type -q bat
+        bat $argv
+    else if type -q batcat
+        batcat $argv
+    else
+        echo "'__bat' expected 'bat' or 'batcat' to be available; got neither" >&2; and return 1
+    end
 end
--- configs/fd/shell.fish
+++ configs/fd/shell.fish
@@ -1,22 +1,40 @@
 #!/usr/bin/env fish
 
-if not status is-interactive; or not type -q fd
+if not status is-interactive; or not begin
+        type -q fd; or type -q fdfind
+    end
     exit
 end
 
 ###############################################################################
 
+if type -q fdfind and not type -q fd
+    function fd
+        __fd $argv
+    end
+end
+
 function fdd
-    __fd_base directory $argv
+    __fd_type directory $argv
 end
 
 function fdf
-    __fd_base file $argv
+    __fd_type file $argv
 end
 
-function __fd_base
+function __fd
+    if type -q fd
+        fd $argv
+    else if type -q fdfind
+        fdfind $argv
+    else
+        echo "'__fd' expected 'fd' or 'fdfind' to be available; got neither" >&2; and return 1
+    end
+end
+
+function __fd_type
     if test (count $argv) -lt 1
         echo "'__fd_base' expected [1..) arguments TYPE; got $(count $argv)" >&2; and return 1
     end
-    fd --hidden --type=$argv[1] $argv[2..]
+    __fd --hidden --type=$argv[1] $argv[2..]
 end
